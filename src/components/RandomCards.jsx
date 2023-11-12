971895a339da7910d7865de1f61886e54deb71d2
Hajurian <julianmoet1@gmail.com>
2023-11-12 16:10:10 -0500

    More song selection work

--- src/components/RandomCards.jsx
+++ src/components/RandomCards.jsx
@@ -1,17 +1,34 @@
-import Cards from './Cards';
+import ScrapedData from '../songdata.json'
 function RandomCards(props) {
+    const data = [];
+    data.push({ ScrapedData });
+    const songList = data[0].ScrapedData;
+    const songs = [];
+    for (let i = 0; i < 5; i++) {
+        let v = props.versions[Math.floor(Math.random() * props.versions.length)]
+        songs.push(songList[v][Math.floor(Math.random() * songList[v].length)]);
+    }
+    
+    function RandomCard(props) {
+        return (
+            <div className="randomcard">
+                <div className="topcard">
+                    <img className="thumbnail" src={props.image}></img>
+                    <h1 className="title">{props.title}</h1>
+                </div>
+            </div>
+        )
+    }
     return(
         <div className="randomcardcontainer">
-            <RandomCard />
-            <RandomCard />
+            <RandomCard image={songs[0].image} title={songs[0].title}/>
+            <RandomCard image={songs[1].image} title={songs[1].title}/>
+            <RandomCard image={songs[2].image} title={songs[2].title}/>
+            <RandomCard image={songs[3].image} title={songs[3].title}/>
+            <RandomCard image={songs[4].image} title={songs[4].title}/>
+            
         </div>
     )
 }
-function RandomCard(props) {
-    return (
-        <>
-            <h1>hi</h1>
-        </>
-    )
-}
+
 export default RandomCards;
