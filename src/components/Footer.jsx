2ee4d7baf177ebaf24aea2407ef008a86c455acc
Hajurian <julianmoet1@gmail.com>
2023-11-29 21:24:22 -0500

    Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,18 @@ import './Footer.css'
 function Footer() {
     return(
         <div className="footercontainer">
-            Fish
+            <div className="footertext">
+                <p>
+                    This website was a passion project that was made from my love for DDR. This is my first major website development project and thus it is by no means professional in any regard. 
+                    <b>I do not claim to own any of the rights to any media I may have used in the development of the website and all rights belong to their respective creators. </b>
+                    All of the files used in this project can be found on this sites github repo <a href="https://github.com/Hajurian/ddrsearch">here</a>. 
+                    <br></br>
+                    With that being said, I'd like to thank my girlfriend, Katie, for helping me design this website (she picked the colors and helped with layouts).
+                    <br></br>
+                    Some of my other work can be found <a href="https://github.com/Hajurian">here</a>
+                    <br></br>
+                </p>
+            </div>
         </div>
     );
 }
